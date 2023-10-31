1fca86a7d4ebac636fd7a50476975183a79fa462
Ludvig Larsson <ludviglarsson01@live.se>
2023-10-31 14:04:04 +0100

    Updated splitter

--- src/components/splitter/splitter.jsx
+++ src/components/splitter/splitter.jsx
@@ -1,10 +1,14 @@
 import styles from './splitter.module.css';
 
-export default function Splitter({ color, height }) {
+export default function Splitter({ colors = ["var(--gray)", "var(--light-gray)", "white"], flip = false }) {
+    if (flip) {
+        colors = colors.reverse();
+    }
     return (
-        <div className={styles.splitter} style={{ 
-            borderLeftColor: color ? color : "#fff", 
-            borderWidth: (height ? height : "10vh") + " 0 0 100vw"
-        }} />
+        <div className={styles.splitter} style={{backgroundColor: colors[0]}}>
+            <div style={{backgroundColor: colors[1]}}>
+                <div style={{backgroundColor: colors[2]}} />
+            </div>
+        </div>
     )
 }
